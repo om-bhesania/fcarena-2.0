28ea03e95742ffaff5fb8e6bc3d14e67a9498e5c
om-bhesania <bhesaniaom@gmail.com>
2024-02-01 22:51:21 +0530

    added build file

--- src/components/BookingPageSections/test.jsx
+++ src/components/BookingPageSections/test.jsx
@@ -2,9 +2,11 @@ import { data } from "autoprefixer";
 import React from "react";
 
 const Test = () => {
-  fetch("https://fcarena-2-0-server.vercel.app")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+ fetch("https://crossorigin.me/https://fcarena-2-0-server.vercel.app")
+   .then((res) => res.json())
+   .then((data) => console.log(data))
+   .catch((error) => console.error("Error fetching data:", error));
+
 
   return <div>{data}</div>;
 };
